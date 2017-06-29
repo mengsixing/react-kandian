eb2ceff1b877d3a430ccbad279fbede310b2ca92
yhlmmm <yinhengli@sina.com>
2017-06-29 16:34:03 +0800

    首页简单布局

--- kandian/src/containers/home/index.js
+++ kandian/src/containers/home/index.js
@@ -3,7 +3,10 @@
  */
 import React from 'react'
 import Header from '../../compontents/header/header'
-import { Button } from 'antd-mobile'
+import { Tabs,List } from 'antd-mobile'
+const TabPane = Tabs.TabPane;
+const Item = List.Item;
+const Brief = Item.Brief;
 
 
 class Home extends React.Component {
@@ -12,8 +15,23 @@ class Home extends React.Component {
     render() {
         return <div>
             <Header />
-            <Button className="btn" type="primary">primary button</Button>
-            <p>home</p>
+            <Tabs>
+                <TabPane tab="头条" key="1"></TabPane>
+                <TabPane tab="社会" key="2"></TabPane>
+                <TabPane tab="国内" key="3"></TabPane>
+                <TabPane tab="国际" key="4"></TabPane>
+                <TabPane tab="娱乐" key="5"></TabPane>
+                <TabPane tab="体育" key="6"></TabPane>
+                <TabPane tab="军事" key="7"></TabPane>
+                <TabPane tab="科技" key="8"></TabPane>
+                <TabPane tab="财经" key="9"></TabPane>
+                <TabPane tab="时尚" key="10"></TabPane>
+            </Tabs>
+            <List className="my-list">
+                <Item extra="10:30" align="top" thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png" multipleLine>
+                    标题文字 <Brief>副标题  副标题  副标题</Brief>
+                </Item>
+            </List>
         </div>;
     }
 }
