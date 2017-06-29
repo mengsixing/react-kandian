57ba18647b8a6127a8cfef73949ffcb61d68c5d3
yhl <yinhengli@sina.com>
2017-06-29 21:49:15 +0800

    首页大体布局实现

--- kandian/src/containers/home/index.js
+++ kandian/src/containers/home/index.js
@@ -3,35 +3,142 @@
  */
 import React from 'react'
 import Header from '../../compontents/header/header'
-import { Tabs,List } from 'antd-mobile'
+import {
+    Tabs,
+    List,
+    Flex,
+    WhiteSpace,
+    Carousel
+} from 'antd-mobile'
+import axios from 'axios';
+import './index.css'
 const TabPane = Tabs.TabPane;
 const Item = List.Item;
 const Brief = Item.Brief;
+axios.defaults.baseURL = 'http://211.149.160.35';
 
+var that;
 
 class Home extends React.Component {
-    componentWillMount(){
+    constructor() {
+        super();
+        that = this;
+        this.state = {
+            tagList: [{}],
+            bannerList: [],
+            newsList: [],
+            data: [1, 2, 3]
+        }
+        //获取头部tags信息
+        axios({
+            method: 'get',
+            url: '/api/cate/cate_list'
+        }).then(function (response) {
+            if (response.data.code === 0) {
+                //循环绑定data active
+                for (var item of response.data.data) {
+                    item.active = false;
+                }
+                that.setState({
+                    tagList: response.data.data
+                });
+            }
+        }).then(function () {
+            //获取新闻列表信息
+            var cid = that.state.tagList[0].id;
+            axios.get('/api/news/news_list?cid=' + cid)
+                .then(function (response) {
+                    if (response.data.code === 0) {
+                        that.setState({
+                            newsList: response.data.data.dataList
+                        });
+                        //绑定IScroll
+                        // that.$nextTick(function () {
+                        //     that.scroller = new BScroll(that.$refs.newsListWrapper, {
+                        //         click: true,
+                        //         scrollbars: true
+                        //     });
+                        //     that.scroller.on("scrollEnd", function () {
+                        //         if (that.scroller.maxScrollY == that.scroller.y &&that.canScroll) {
+                        //             //获取选中的标签id
+                        //             let tagId = that.tagList.filter(item => !!item.active)[0].id;
+                        //             let offset=(that.pageIndex+1) * 10;
+                        //             that.$http.get('/api/news/news_list?cid=' + tagId + '&offset=' + offset).then(function (response) {
+                        //                 if (response.data.code == 0) {
+                        //                     if(response.data.data.dataList.length>0){
+                        //                         that.pageIndex++;
+                        //                         that.newsList = that.newsList.concat(response.data.data.dataList);
+                        //                         that.$nextTick(function () {
+                        //                             that.scroller.refresh();
+                        //                         });
+                        //                     }
+                        //                 }
+                        //             });
+                        //         }
+                        //     });
+                        //
+                        //
+                        // });
+                    }
+                });
+        });
+        //获取banner广告信息
+        axios.get('/api/face/face_list').then(function (response) {
+            if (response.data.code === 0) {
+                that.setState({
+                    bannerList:response.data.data.filter(item=>item.is_show === '1')
+                });
+            }
+        });
     }
+
+    componentWillMount() {
+    }
+
     render() {
+        var tabpanes = this.state.tagList.map((item, index) => (
+            <TabPane tab={item.name} key={index}></TabPane>
+        ));
+        var bannerLists=this.state.bannerList.map((item, index) => (
+            <a href={item.url} key={index}>
+                <img
+                    src={item.pic}
+                    alt="icon"
+                />
+            </a>
+        ));
+        var newsLists=this.state.newsList.map((item, index) => (
+            <Item key={index} align="top"
+                  thumb={item.litpic} multipleLine>
+                {item.title}
+                <Brief>
+                    <Flex justify="between">
+                        <Flex.Item>{item.news_from}</Flex.Item>
+                        <Flex.Item>{item.price}K币</Flex.Item>
+                        <Flex.Item>阅读：{item.click}</Flex.Item>
+                    </Flex>
+                </Brief>
+            </Item>
+        ));
         return <div>
             <Header />
             <Tabs>
-                <TabPane tab="头条" key="1"></TabPane>
-                <TabPane tab="社会" key="2"></TabPane>
-                <TabPane tab="国内" key="3"></TabPane>
-                <TabPane tab="国际" key="4"></TabPane>
-                <TabPane tab="娱乐" key="5"></TabPane>
-                <TabPane tab="体育" key="6"></TabPane>
-                <TabPane tab="军事" key="7"></TabPane>
-                <TabPane tab="科技" key="8"></TabPane>
-                <TabPane tab="财经" key="9"></TabPane>
-                <TabPane tab="时尚" key="10"></TabPane>
+                {tabpanes}
             </Tabs>
+
+            <Carousel
+                className="my-carousel"
+                autoplay={false}
+                infinite
+                selectedIndex={1}
+                swipeSpeed={35}
+            >
+                {bannerLists}
+            </Carousel>
             <List className="my-list">
-                <Item extra="10:30" align="top" thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png" multipleLine>
-                    标题文字 <Brief>副标题  副标题  副标题</Brief>
-                </Item>
+                {newsLists}
             </List>
+
         </div>;
     }
 }
