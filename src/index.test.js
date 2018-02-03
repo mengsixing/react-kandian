5746a33ea9af9b9ce02b8d37619cc11eeed326b9
尹珩力 <yinhengli@sina.com>
2018-02-03 23:16:33 +0800

    上传test代码

---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './router/router';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+});
